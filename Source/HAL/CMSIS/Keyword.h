775d9bd51c9ab0ce0016568b531ddb65a6891667
DoQuocViet1110 <dqvietc6.pbchau@gmail.com>
2023-09-30 21:45:02 +0700

    Update Keyword.h

--- Source/HAL/CMSIS/Keyword.h
+++ Source/HAL/CMSIS/Keyword.h
@@ -1,21 +1,16 @@
-typedef signed char tS8;
-typedef signed short tS16;
-typedef signed long tS32;
-typedef signed long long tS64;
-typedef unsigned char tU8;
-typedef unsigned short tU16;
-typedef unsigned long tU32;
-typedef unsigned long long tU64;
-typedef float tF32;
-typedef double tF64;
-
-typedef tU8 bit;
-
-typedef int tINT;
-typedef unsigned tUINT;
+typedef signed char          S08;
+typedef signed short         S16;
+typedef signed long          S32;
+typedef signed long long     S64;
+typedef unsigned char        U08;
+typedef unsigned short       U16;
+typedef unsigned long        U32;
+typedef unsigned long long   U64;
+typedef float                F32;
+typedef double               F64;
 
 typedef enum
 {
-    False = (0 == 1),
-    True = (0 == 0)
-} tBool;
+    FLASE,
+    TRUE
+} BOOL;
