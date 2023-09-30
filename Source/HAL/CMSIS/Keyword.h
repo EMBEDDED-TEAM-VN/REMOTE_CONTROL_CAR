343661bde5a00d5d29618a3034872d409b9db5f3
DoQuocViet1110 <dqvietc6.pbchau@gmail.com>
2023-09-30 20:42:46 +0700

    Implement code for Keyword.h file

---
+++ Source/HAL/CMSIS/Keyword.h
@@ -0,0 +1,21 @@
+typedef signed char tS8;
+typedef signed short tS16;
+typedef signed long tS32;
+typedef signed long long tS64;
+typedef unsigned char tU8;
+typedef unsigned short tU16;
+typedef unsigned long tU32;
+typedef unsigned long long tU64;
+typedef float tF32;
+typedef double tF64;
+
+typedef tU8 bit;
+
+typedef int tINT;
+typedef unsigned tUINT;
+
+typedef enum
+{
+    False = (0 == 1),
+    True = (0 == 0)
+} tBool;
