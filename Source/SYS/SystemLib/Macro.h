153dca21c2778984c46766277d3d9e761aab3cc2
hienprogramming <bachhien500@gmail.com>
2023-09-30 16:04:50 +0700

    Creating Macro file

---
+++ Source/SYS/SystemLib/Macro.h
@@ -0,0 +1,17 @@
+//--- Standard Macro Define ---//
+#define FALSE       0
+#define TRUE        1
+#define OFF         0
+#define ON          1
+#define LOW         0
+#define HIGH        1
+#define NO_OK       0
+#define OK          1
+#define INIT        0   // Initialize of the Car
+#define RUNNING     1   // Starting for running
+#define EXACTLY     2   // The Car is Stable
+#define MODE        0   // If Mode is equal 0 -> controller by Remote. Else is 1 -> automation Mode
+#define START       1
+#define STOP        0
+#define DESCREASE   0
+#define INSCREASE   1
